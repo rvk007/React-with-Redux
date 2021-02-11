c1f9746dcf86a6ec09629007bb8e18ee22017ab0
Rakhee <rksharma19896@gmail.com>
2021-02-12 01:06:21 +0530

    Added Youtube api call

--- pics/src/api/unsplash.js
+++ pics/src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'https://api.unsplash.com/',
   headers: {
-    Authorization: 'Client-ID tg2G6friEANFTqHrQ0OjY1KWEuRTGRL7oqrn4cuF-2k',
+    Authorization: `Client-ID ${process.env.REACT_APP_UNSPLASH_API_KEY}`,
   },
 });
