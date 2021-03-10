4da4c8f6266aac5b58f0b10d9521f892a5a05d00
Rakhee <rksharma19896@gmail.com>
2021-03-10 23:08:17 +0530

    Added Songs React-Redux Project

---
+++ songs/src/reducers/index.js
@@ -0,0 +1,35 @@
+import { combineReducers } from 'redux';
+
+// Reducers
+const songsReducer = () => {
+  return [
+    {
+      title: 'No Scrubs',
+      duration: '4:05',
+    },
+    {
+      title: 'Macarena',
+      duration: '2:13',
+    },
+    {
+      title: 'All Star',
+      duration: '3:15',
+    },
+    {
+      title: 'I want it that way',
+      duration: '5:25',
+    },
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === 'SONG_SELECTED') {
+    return action.payload;
+  }
+  return selectedSong;
+};
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer,
+});
