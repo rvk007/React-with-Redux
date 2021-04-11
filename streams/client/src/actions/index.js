455c2a728d4426d3c9466e22f4d30af48e33d1d2
Rakhee <rksharma19896@gmail.com>
2021-04-11 12:25:16 +0530

    Setting up StreamDelete screen

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -8,7 +8,7 @@ import {
   EDIT_STREAM,
 } from './types';
 import streams from '../apis/streams';
-import StreamShow from '../components/streams/StreamShow';
+import history from '../history';
 
 export const signIn = userId => {
   return {
@@ -27,6 +27,8 @@ export const createStream = formValues => async (dispatch, getState) => {
   const { userId } = getState().auth;
   const response = await streams.post('/streams', { ...formValues, userId });
   dispatch({ type: CREATE_STREAM, payload: response.data });
+  //Programmatic navigation
+  history.push('/');
 };
 
 export const fetchStreams = () => async dispatch => {
@@ -42,9 +44,13 @@ export const fetchStream = id => async dispatch => {
 export const deleteStream = id => async dispatch => {
   await streams.delete(`/streams/${id}`);
   dispatch({ type: DELETE_STREAM, payload: id });
+  //Programmatic navigation
+  history.push('/');
 };
 
 export const editStream = (id, formValues) => async dispatch => {
-  const response = await streams.put(`/streams/${id}`, formValues);
+  const response = await streams.patch(`/streams/${id}`, formValues);
   dispatch({ type: EDIT_STREAM, payload: response.data });
+  //Programmatic navigation
+  history.push('/');
 };
