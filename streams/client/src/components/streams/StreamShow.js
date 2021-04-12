86e17db2a855a4771e0e29daf07b693d1429fef4
Rakhee <rksharma19896@gmail.com>
2021-04-12 19:38:45 +0530

    Added RMTP server to show live streaming

--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchStream } from '../../actions';
+import flv from 'flv.js';
 
 class StreamShow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.videoRef = React.createRef();
+  }
+
   componentDidMount() {
-    this.props.fetchStream(this.props.match.params.id);
+    const { id } = this.props.match.params;
+    this.props.fetchStream(id);
+    this.buildPlayer();
   }
 
   renderDetails() {
@@ -14,12 +22,34 @@ class StreamShow extends React.Component {
     const { title, description } = this.props.streams;
     return (
       <div>
+        <video ref={this.videoRef} style={{ width: '100%' }} controls />
         <h1 className="header">{title}</h1>
         <h5 className="description">{description}</h5>
       </div>
     );
   }
 
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  componentWillUnmount() {
+    this.flvPlayer.destroy();
+  }
+
+  buildPlayer() {
+    if (this.flvPlayer || !this.props.streams) {
+      return;
+    }
+
+    const { id } = this.props.match.params;
+    this.flvPlayer = flv.createPlayer({
+      type: 'flv',
+      url: `http://localhost:8000/live/${id}.flv `,
+    });
+    this.flvPlayer.attachMediaElement(this.videoRef.current);
+    this.flvPlayer.load();
+  }
   render() {
     return <div>{this.renderDetails()}</div>;
   }
